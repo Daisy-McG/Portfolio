344a885994e5a4385588d044814f7b073d76bde5
Daisy-McG <mcgirr.daisy@gmail.com>
2022-04-03 22:07:23 +0100

    Remove unused import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
-import styled from 'styled-components/macro'
 
 import { Navbar } from './components/navigation'
 

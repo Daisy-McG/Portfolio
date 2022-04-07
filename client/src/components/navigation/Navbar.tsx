8a1bc670f2628ac8ddaf51e4f7da005f215b81ef
Daisy-McG <mcgirr.daisy@gmail.com>
2022-04-07 23:38:32 +0100

    Style navbar with drop shadow & bg color

--- client/src/components/navigation/Navbar.tsx
+++ client/src/components/navigation/Navbar.tsx
@@ -6,9 +6,10 @@ import '../../theme/theme.css'
 const StyledNav = styled.div`
     display: flex;
     flex-direction: column;
-    background-color: var(--purple);
+    background-color: var(--blue);
     align-items: center;
     justify-content: space-between;
+    box-shadow: 2px 3px 4px #4e4747;
     @media (min-width: 50rem) {
     flex-direction: row;
   }
